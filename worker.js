c8365a3d3de7b029cff2dfa540960fa53c9d43c8
hostedmailers <hostedmailers@gmail.com>
2025-11-25 20:37:37 -0500

    feat: Add landing page and associated assets, update routing logic

--- worker.js
+++ worker.js
@@ -3,6 +3,8 @@ const ROUTES = [
   { prefix: "/big", assetBase: "/big" }
 ];
 
+const LANDING_PREFIX = "/landing";
+
 export default {
   async fetch(request, env) {
     const originalUrl = new URL(request.url);
@@ -18,6 +20,16 @@ export default {
 
       const assetRequest = new Request(rewrittenUrl.toString(), request);
       return env.ASSETS.fetch(assetRequest);
+    }
+
+    if (!matchesPrefix(originalUrl.pathname, LANDING_PREFIX)) {
+      const landingUrl = new URL(originalUrl);
+      landingUrl.pathname = `${LANDING_PREFIX}${normalizeSubPath(originalUrl.pathname)}`;
+      const landingRequest = new Request(landingUrl.toString(), request);
+      const landingResponse = await env.ASSETS.fetch(landingRequest);
+      if (landingResponse.status !== 404) {
+        return landingResponse;
+      }
     }
 
     return env.ASSETS.fetch(request);
